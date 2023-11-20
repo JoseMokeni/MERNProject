f5b7b76af2a5d3b9277d3dd153ebb5934c85e3f8
José Mokeni <jmmokeni@gmail.com>
2023-11-20 22:20:30 +0100

    Auth middleware implemented for productRoutes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,343 +1,291 @@
-const Product = require('../models/Product');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
-const cloudinary = require('cloudinary').v2
-const fs = require('fs');
-
+const Product = require("../models/Product");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 // @desc: Add a new product
 // @route: POST /api/products
 // @access: Private
 
 const addProduct = asyncHandler(async (req, res) => {
-    // check if one of the fields is empty
-    const fieldsEmpty = !req.body.name || !req.body.price || !req.body.description || !req.files.image || !req.body.category
+  // check if one of the fields is empty
+  const fieldsEmpty =
+    !req.body.name ||
+    !req.body.price ||
+    !req.body.description ||
+    !req.files.image ||
+    !req.body.category;
 
-    if (fieldsEmpty) {
-        res.status(400)
-        throw new Error('Please fill all the fields')
-    }
+  if (fieldsEmpty) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
 
-    let {
-        name,
-        price,
-        description,
-        category
-    } = req.body
+  let { name, price, description, category } = req.body;
 
-    if (!tokenSended(req)){
-        res.status(401)
-        throw new Error('Invalid token')
-    }
-    
-    const token = req.headers.authorization.split(' ')[1]
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  const owner = req.user._id;
 
-    if (!decoded) {
-        res.status(401)
-        throw new Error('Invalid token')
-    }
+  // get the image file
+  const image = req.files.image;
 
-    const owner = decoded._id
+  // upload image to cloudinary
+  const uploadedImage = await cloudinary.uploader.upload(image.tempFilePath);
+  imageURL = uploadedImage.secure_url;
 
-    // get the image file   
-    const image = req.files.image
-    
-    // upload image to cloudinary
-    const uploadedImage = await cloudinary.uploader.upload(image.tempFilePath)
-    imageURL = uploadedImage.secure_url
+  const productToAdd = {
+    name,
+    price,
+    description,
+    image: imageURL,
+    owner,
+    category,
+  };
 
-    const productToAdd = {
-        name,
-        price,
-        description,
-        image: imageURL,
-        owner,
-        category
-    }
+  const product = await Product.create(productToAdd);
 
-    const product = await Product.create(productToAdd)
+  if (product) {
+    res.status(201);
+    // remove temp file
+    fs.unlinkSync(image.tempFilePath);
 
-    if (product) {
-        res.status(201)
-        // remove temp file
-        fs.unlinkSync(image.tempFilePath);
-
-        res.json({
-            message: 'Product added successfully'
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid product data')
-    }
-
-})
+    res.json({
+      message: "Product added successfully",
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid product data");
+  }
+});
 
 // @desc: Get all products
 // @route: GET /api/products
 // @access: Public
 
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({})
+  const products = await Product.find({});
 
-    if (products) {
-        res.status(200)
-        res.json(products)
-    } else {
-        res.status(404)
-        throw new Error('No products found')
-    }
-})
+  if (products) {
+    res.status(200);
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error("No products found");
+  }
+});
 
 // @desc: Get a product by id
 // @route: GET /api/products/:id
 // @access: Public
 
 const getProductById = asyncHandler(async (req, res) => {
-    const id = req.params.id
-    if (!id) {
-        res.status(400)
-        throw new Error('Please provide an id')
-    }
+  const id = req.params.id;
+  if (!id) {
+    res.status(400);
+    throw new Error("Please provide an id");
+  }
 
-    const product = await Product.findById(id)
+  const product = await Product.findById(id);
 
-    if (product) {
-        res.status(200)
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('No product found')
-    }
-})
+  if (product) {
+    res.status(200);
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("No product found");
+  }
+});
 
 // @desc: Get products by category
 // @route: GET /api/products/category/:category
 // @access: Public
 
 const getProductsByCategory = asyncHandler(async (req, res) => {
-    
-    const category = req.params.category
-    if (!category) {
-        res.status(400)
-        throw new Error('Please provide a category')
-    }
+  const category = req.params.category;
+  if (!category) {
+    res.status(400);
+    throw new Error("Please provide a category");
+  }
 
-    const products = await Product.find({category})
+  const products = await Product.find({ category });
 
-    if (products) {
-        res.status(200)
-        res.json(products)
-    } else {
-        res.status(404)
-        throw new Error('No products found for this category')
-    }
-})
+  if (products) {
+    res.status(200);
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error("No products found for this category");
+  }
+});
 
 // @desc: Get my products
 // @route: GET /api/products/my-products
 // @access: Private
 
 const getMyproducts = asyncHandler(async (req, res) => {
-    if (!tokenSended(req)){
-        res.status(401)
-        throw new Error('Invalid token')
-    }
-    const token = req.headers.authorization.split(' ')[1]
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  const owner = req.user._id;
 
-    if (!decoded) {
-        res.status(401)
-        throw new Error('Invalid token')
-    }
+  // fetch the database
+  const products = await Product.find({ owner });
 
-    const owner = decoded._id
-
-    // fetch the database
-    const products = await Product.find({owner})
-    
-    if (products) {
-        res.status(200)
-        res.json(products)
-    } else {
-        res.status(404)
-        throw new Error('No products found')
-    }
-
-})
+  if (products) {
+    res.status(200);
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error("No products found");
+  }
+});
 
 // @desc: Get products by owner
 // @route: GET /api/products/owner/:id
 // @access: Public
 
 const getProductsByOwner = asyncHandler(async (req, res) => {
-    const id = req.params.id
-    if (!id) {
-        res.status(400)
-        throw new Error('Please provide an id')
-    }
+  const id = req.params.id;
+  if (!id) {
+    res.status(400);
+    throw new Error("Please provide an id");
+  }
 
-    // fetch the database
-    const products = await Product.find({owner: id})
-    
-    if (products) {
-        res.status(200)
-        res.json(products)
-    } else {
-        res.status(404)
-        throw new Error('No products found')
-    }
+  // fetch the database
+  const products = await Product.find({ owner: id });
 
-})
-
+  if (products) {
+    res.status(200);
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error("No products found");
+  }
+});
 
 // @desc: Delete a product
 // @route: DELETE /api/products/:id
 // @access: Private
 
 const deleteProduct = asyncHandler(async (req, res) => {
-    if (!tokenSended(req)){
-        res.status(401)
-        throw new Error('Invalid token')
-    }
-    const token = req.headers.authorization.split(' ')[1]
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  const id = req.params.id;
 
-    console.log("Decoded")
-    console.log(decoded)
+  console.log("id: " + id);
 
-    const id = req.params.id
+  if (!id) {
+    res.status(400);
+    throw new Error("Please provide an id");
+  }
 
-    console.log("id: " + id);
+  const product = await Product.findById(id);
 
-    if (!id) {
-        res.status(400)
-        throw new Error('Please provide an id')
-    }
+  if (!product) {
+    res.status(404);
+    throw new Error("No product found");
+  }
 
-    const product = await Product.findById(id)
+  if (req.user._id != product.owner && req.user.role !== "admin") {
+    res.status(401);
+    throw new Error("Invalid token");
+  }
 
-    if (!product) {
-        res.status(404)
-        throw new Error('No product found')
-    }
+  // delete the product
+  const removedProduct = await Product.findByIdAndDelete(id);
+  if (removedProduct) {
+    res.status(200);
 
-    if (decoded._id != product.owner && decoded.role !== 'admin') {
-        res.status(401)
-        throw new Error('Invalid token')
-    }
+    // delete the image from cloudinary
+    const image = removedProduct.image;
+    const imageId = image.split("/").pop().split(".")[0];
+    await cloudinary.uploader.destroy(imageId);
 
-    // delete the product
-    const removedProduct = await Product.findByIdAndDelete(id)
-    if (removedProduct) {
-        res.status(200)
-    
-        res.json({
-            message: 'Product deleted successfully'
-        })
-    } else {
-        res.status(404)
-        throw new Error('An error occured while deleting the product')
-    
-    }
-
-})
+    res.json({
+      message: "Product deleted successfully",
+    });
+  } else {
+    res.status(404);
+    throw new Error("An error occured while deleting the product");
+  }
+});
 
 // @desc: Update product
 // @route: PUT /api/products/:id
 // @access: PRIVATE
 
 const updateProduct = asyncHandler(async (req, res) => {
-    if (!tokenSended(req)){
-        res.status(401)
-        throw new Error('Invalid token')
-    }
-    const token = req.headers.authorization.split(' ')[1]
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  const id = req.params.id;
 
-    console.log("Decoded")
-    console.log(decoded)
+  if (!id) {
+    res.status(400);
+    throw new Error("Please provide an id");
+  }
 
-    const id = req.params.id
+  const product = await Product.findById(id);
 
-    console.log("id: " + id);
+  if (!product) {
+    res.status(404);
+    throw new Error("No product found with this id");
+  }
 
-    if (!id) {
-        res.status(400)
-        throw new Error('Please provide an id')
-    }
+  if (req.user._id != product.owner && req.user.role !== "admin") {
+    res.status(401);
+    throw new Error("Invalid token");
+  }
 
-    const product = await Product.findById(id)
+  const newData = {
+    name: req.body.name || product.name,
+    price: req.body.price || product.price,
+    description: req.body.description || product.description,
+    category: req.body.category || product.category,
+  };
 
-    if (!product) {
-        res.status(404)
-        throw new Error('No product found with this id')
-    }
+  // image
+  if (req.files) {
+    const image = req.files.image;
+    const uploadedImage = await cloudinary.uploader.upload(image.tempFilePath);
+    imageURL = uploadedImage.secure_url;
+    newData.image = imageURL;
+  } else {
+    newData.image = product.image;
+  }
 
-    if (decoded._id != product.owner && decoded.role !== 'admin') {
-        res.status(401)
-        throw new Error('Invalid token')
-    }
+  // update the product
+  product.name = newData.name;
+  product.price = newData.price;
+  product.description = newData.description;
+  product.category = newData.category;
+  product.image = newData.image;
 
-    const newData = {
-        name: req.body.name || product.name,
-        price: req.body.price || product.price,
-        description: req.body.description || product.description,
-        category: req.body.category || product.category
-    }
+  const updatedProduct = await product.save();
 
-    // image
-    if (req.files) {
-        const image = req.files.image
-        const uploadedImage = await cloudinary.uploader.upload(image.tempFilePath)
-        imageURL = uploadedImage.secure_url
-        newData.image = imageURL
-    } else {
-        newData.image = product.image
-    }
-
-    // update the product
-    product.name = newData.name
-    product.price = newData.price
-    product.description = newData.description
-    product.category = newData.category
-    product.image = newData.image
-
-    const updatedProduct = await product.save()
-
-    if (updatedProduct) {
-        res.status(200)
-    
-        res.json({
-            message: 'Product updated successfully'
-        })
-    } else {
-        res.status(404)
-        throw new Error('An error occured while updating the product')
-    
-    }
-
-    
-})
+  if (updatedProduct) {
+    res.status(200);
+    // remove temp file
+    fs.unlinkSync(image.tempFilePath);
+    res.json({
+      message: "Product updated successfully",
+    });
+  } else {
+    res.status(404);
+    throw new Error("An error occured while updating the product");
+  }
+});
 
 const tokenSended = (req) => {
-    // verify if the token is present in the headers
-    const authorization = req.headers.authorization
-    if (!authorization) {
-        return false
-    } 
+  // verify if the token is present in the headers
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return false;
+  }
 
-    return true
-}
+  return true;
+};
 
 module.exports = {
-    addProduct,
-    getProducts,
-    getProductById,
-    getMyproducts,
-    deleteProduct,
-    getProductsByOwner,
-    getProductsByCategory,
-    updateProduct
-}
+  addProduct,
+  getProducts,
+  getProductById,
+  getMyproducts,
+  deleteProduct,
+  getProductsByOwner,
+  getProductsByCategory,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { addProduct, getProductById, getProducts, getMyproducts, deleteProduct, getProductsByOwner, getProductsByCategory, updateProduct } = require('../controllers/productController')
+const {
+  addProduct,
+  getProductById,
+  getProducts,
+  getMyproducts,
+  deleteProduct,
+  getProductsByOwner,
+  getProductsByCategory,
+  updateProduct,
+} = require("../controllers/productController");
+const protect = require("../middlewares/authMiddleware");
 
-router.post('/', addProduct)
+router.post("/", protect, addProduct);
 
-router.get('/:id', getProductById)
+router.get("/:id", getProductById);
 
-router.get('/', getProducts)
+router.get("/", getProducts);
 
-router.get('/my-products', getMyproducts)
+router.get("/my-products", protect, getMyproducts);
 
-router.get('/owner/:id', getProductsByOwner)
+router.get("/owner/:id", getProductsByOwner);
 
-router.get('/category/:category', getProductsByCategory)
+router.get("/category/:category", getProductsByCategory);
 
-router.delete('/:id', deleteProduct)
+router.delete("/:id", protect, deleteProduct);
 
-router.put('/:id', updateProduct)
+router.put("/:id", protect, updateProduct);
 
-
-
-module.exports = router
+module.exports = router;
